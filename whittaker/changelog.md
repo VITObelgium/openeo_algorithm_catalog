5b3bf09a907f9114e93aedb5b27ff0f18eea2074
SHARMAP <pratichhya.sharma@vito.be>
2025-11-25 15:31:29 +0100

    whittaker

---
+++ whittaker/changelog.md
@@ -0,0 +1,13 @@
+# Changelog for **Whittaker** Service
+
+### Unreleased:
+
+### 19/06/2025
+
+#### Changed
+- Updated SCL masking from `mask_scl_dilation` to `to_scl_dilation_mask`.
+
+### Initial release
+
+- Migrated the MOGPR service as-is from the FuseTS implementation.
+- The service is available in the marketplace under the namespace: https://openeo.dataspace.copernicus.eu/openeo/processes/u:6391851f-9042-4108-8b2a-3dd2e8a9dd0b/whittaker
--- whittaker/openeo_udp/generate_udp_pg.py
+++ whittaker/openeo_udp/generate_udp_pg.py
@@ -1,24 +1,21 @@
 """ "
-This script generates the OpenEO UDP for the Phenology algorithm.
+This script generates the OpenEO UDP for the Whittaker algorithm.
 Implemented by https://open-eo.github.io/FuseTS/
 Contact: marketplace@terrascope.be
 """
 
-from typing import Union
 from pathlib import Path
 import json
 
 import openeo
-from fusets.openeo.phenology_udf import load_phenology_udf
-from fusets.openeo.services.publish_phenology import phenology_bands
-from openeo import DataCube
+from fusets.openeo import load_whittakker_udf
 from openeo.api.process import Parameter
-from openeo.processes import apply_dimension, ProcessBuilder
+from openeo.processes import apply_dimension
 from openeo.rest.udp import build_process_dict
 
 
 def generate() -> dict:
-    print("Generating UDP for Phenology...")
+    print("Generating UDP for Whittaker...")
     connection = openeo.connect("openeo.dataspace.copernicus.eu")
     print("Defining parameters...")
     spatial_extent = Parameter.spatial_extent(
@@ -28,6 +25,11 @@ def generate() -> dict:
     temporal_extent = Parameter.temporal_interval(
         name="temporal_extent",
         description="Temporal extent specified as two-element array with start and end date/date-time.",
+    )
+    smoothing_lambda = Parameter.integer(
+        name="smoothing_lambda",
+        description="Smoothing parameter for the Whittaker smoother. Higher values result in a smoother time series. Typical values are between 10 and 100000.",
+        default=10000,
     )
     print("Loading data...")
     cube = connection.load_collection(
@@ -52,28 +54,29 @@ def generate() -> dict:
     print("Calculating NDVI...")
     base_ndvi = cube.ndvi(red="B04", nir="B08")
 
-    phenology = apply_dimension(
-        data=base_ndvi,
+    whittaker_cube = apply_dimension(
+        base_ndvi,
         process=lambda x: x.run_udf(
             udf=(
                 Path(__file__).parent.parent.parent / "utils" / "set_dependency_path.py"
             ).read_text()
             + "\n"
-            + load_phenology_udf(),
+            + load_whittakker_udf(),
             runtime="Python",
             version="3.8",
+            context={"smoothing_lambda": smoothing_lambda},
         ),
         dimension="t",
-        target_dimension="phenology",
     )
+    print("Applying Whittaker smoothing...")
 
-    phenology = phenology.add_dimension("var", phenology_bands[0], "bands")
-    phenology = phenology.rename_labels(dimension="var", target=phenology_bands)
+    # Calculate the average time series value for the given area of interest
+    whittaker = whittaker_cube.aggregate_spatial(spatial_extent, reducer="mean")
 
     return build_process_dict(
-        process_graph=phenology,
-        process_id="phenology",
-        summary="Calculate phenology metrics from Sentinel-2 NDVI",
+        process_graph=whittaker,
+        process_id="whittaker",
+        summary="Calculate Whittaker smoothing from Sentinel-2 NDVI",
         description=(Path(__file__).parent / "readme.md").read_text(),
         parameters=[
             spatial_extent,
@@ -84,6 +87,6 @@ def generate() -> dict:
 
 if __name__ == "__main__":
     # save the generated process to a file
-    with open("phenology.json", "w") as f:
+    with open("whittaker.json", "w") as f:
         json.dump(generate(), f, indent=2)
-        print("UDP 'phenology.json' generated.")
+        print("UDP 'whittaker.json' generated.")
